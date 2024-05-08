99c5f49d607b14f80fb6c541f921987ce2686164
manybotts <105394368+manybotts@users.noreply.github.com>
2024-05-08 19:35:35 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
-git clone https://github.com/manybotts/iboxFilter /iboxFilter
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/manybotts/iboxFilter /iboxFilter
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /iboxFilter
+fi
 cd /iboxFilter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
