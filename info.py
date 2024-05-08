c20cb90c83fd658489f2d26148333c62290e590c
manybotts <105394368+manybotts@users.noreply.github.com>
2024-05-08 19:41:46 +0300

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'iboxcollection')
 
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'BgnavQ2FIXamyq8sgdF7aPkhLgA3)
+SHORTLINK_API = environ.get('SHORTLINK_API', 'BgnavQ2FIXamyq8sgdF7aPkhLgA3')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "10")
