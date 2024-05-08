940b24676c5f5d7346f433163b5acbea1ac071b0
manybotts <105394368+manybotts@users.noreply.github.com>
2024-05-08 18:59:57 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-Auto-Filter /Advance-Auto-Filter
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-Auto-Filter
-fi
-cd /Advance-Auto-Filter
+cd /iboxFilter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
